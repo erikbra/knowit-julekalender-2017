76e01cfe0ca1defc79e03e1086e99d3b7931d425
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2017-12-02 21:30:26 +0100

    Working on it... more stats

--- luke1/PowerShell/solve.ps1
+++ luke1/PowerShell/solve.ps1
@@ -1,27 +1,52 @@
-#! /usr/bin/env/pwsh
+#! /usr/bin/env pwsh
 
 #$perm = "aeteesasrsssstaesersrrsse";
 $word1 = "misiststltleletetotoe"
 $word2 = "snnoowwffllaakke"
 $wordList = ".\wordlist.txt";
 
+function N-Gram ($word, $n) {
+   $chars = $word.ToCharArray();
+   $ngram = @();
+   for ($i = 0; $i -lt $chars.Count -$n + 1; $i++) {
+      $end = $i + $n -1
+      $ngram += $chars[$i..$end];
+   } 
+   $ngram -join ""
+}
+
+function Stats ($ngram) {
+    $chars = $ngram.ToCharArray();
+    $sorted = (($chars | Sort-Object))
+    $len = $ngram.Length
+
+    $stats = ($sorted | Group | Sort-Object -Property Count)
+
+    $len
+    $stats
+}
+
+$words = @("snowflake", "mistletoe");
+
+$words | % {
+    for ($i = 2; $i -lt 5; $i++) {
+       $ngram = N-Gram $_ $i 
+       Write-Host "`n$($_), len $($_.Length), n = $($i): $ngram"
+       Stats $ngram
+    }
+}
+
+return
+
+N-Gram "snowflake" 2
+return
 
 @($word1, $word2) | % {
     $perm = $_;
-
-$chars = $perm.ToCharArray();
-$sorted = (($chars | sort))
-$len = $perm.Length
-
-$stats = ($sorted | Group | Sort -Property Count)
-
-$len
-$stats
-
 }
-return;
 
-$unique = (($chars | sort)) | Get-Unique
+
+$unique = $sorted | Get-Unique
 
 if (!(Test-Path $wordList)) {
     Invoke-WebRequest -UseBasicParsing -OutFile $wordList https://s3-eu-west-1.amazonaws.com/julekalender-knowit-2017-vedlegg/wordlist.txt
