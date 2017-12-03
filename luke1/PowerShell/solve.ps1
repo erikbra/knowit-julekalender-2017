affcdba66269fd398ccd9826fd5f897107605832
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2017-12-03 02:37:20 +0100

    Really in a blind track

--- luke1/PowerShell/solve.ps1
+++ luke1/PowerShell/solve.ps1
@@ -1,6 +1,6 @@
 #! /usr/bin/env pwsh
 
-#$perm = "aeteesasrsssstaesersrrsse";
+$perm = "aeteesasrsssstaesersrrsse";
 $word1 = "misiststltleletetotoe"
 $word2 = "snnoowwffllaakke"
 $wordList = ".\wordlist.txt";
@@ -22,28 +22,70 @@ function Stats ($ngram) {
 
     $stats = ($sorted | Group | Sort-Object -Property Count)
 
-    $len
     $stats
 }
 
 $words = @("snowflake", "mistletoe");
+$words = @("abcdefghi");
+$words = @();
 
 $words | % {
-    for ($i = 2; $i -lt 5; $i++) {
+    $maxLen = [Math]::Pow([Math]::Round(1.00 * $_.Length / 2), 2);
+
+    for ($i = 1; $i -lt 10; $i++) {
        $ngram = N-Gram $_ $i 
        Write-Host "`n$($_), len $($_.Length), n = $($i): $ngram"
        Stats $ngram
     }
 }
 
-return
+function Partition([int] $n, $max, $used) {
+    $parts = @()
+    if ($n -eq 0) {
+        return $parts;
+    }
 
-N-Gram "snowflake" 2
-return
-
-@($word1, $word2) | % {
-    $perm = $_;
+    for ($i = 1; $i -le $max; $i++) { 
+        [int] $other = $n - $i 
+        Write-Host "n: $n, max: $max, i: $i, other: $other"
+        $used[$i]+=1;
+        #"used i = $($used)"
+        #if ($used[$i] -ge 2) {
+            #"Continuing - used i = $($used[$i])"
+            #return $parts
+        #}
+        $newMax = $(([int]$other)/2) 
+        $parts += "$n + $(Partition $($other) $newMax $used)"
+   }
+   return $parts
 }
+
+[int[]] $used = new-object int[] 6
+Partition 3 3 $used  | Select-Object
+return
+
+$perms = @("abcdebcdefcdefgdefghefghi", $perm);
+
+$perms | % {
+    $permLen = $($perm.Length);
+    "Perm len: $($permLen)"
+
+    $middle = [Math]::Sqrt($permLen)
+
+    if ($permLen % 2 -eq 0) {
+        $len = $middle * 2;
+    } else {
+        $len = $middle * 2 - 1;
+    }
+
+    "`nWord len: $($len)"
+    Stats $_ | % {
+        $combinations = Partition $_.Count $middle
+        "$($_.Name): $($combinations -join ', ')"
+    }
+}
+
+return
 
 
 $unique = $sorted | Get-Unique
